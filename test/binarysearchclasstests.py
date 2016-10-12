f0f66cb2044d1b22c40e5bcb27ef217f1f0ca952
Michael Kamau <tokamau@gmail.com>
2016-10-12 08:46:49 +0300

    Add test search val in small list

--- test/binarysearchclasstests.py
+++ test/binarysearchclasstests.py
@@ -65,3 +65,28 @@ class ListComprehensionTest(unittest.TestCase):
                     self.ten_to_thousand[index],
                     msg='should return 10 for consequtive numbers'
                 )
+
+
+class BinarySearchTest(unittest.TestCase):
+    """Get the index of the item with an expected number of loops in\
+     array [1, 2 . . . 20]
+       Returns a dictionary containing {count: value, index: value}
+    """
+
+    def setUp(self):
+        self.one_to_twenty = BinarySearch(20, 1)
+        self.two_to_forty = BinarySearch(20, 2)
+        self.ten_to_thousand = BinarySearch(100, 10)
+
+    def test_small_list_search(self):
+        search = self.one_to_twenty.search(16)
+        self.assertGreater(
+            5,
+            search['count'],
+            msg='should return {count: 4, index: 15} for 16'
+        )
+        self.assertEqual(
+            15,
+            search['index'],
+            msg='should return {count: 4, index: 15} for 16'
+        )
