05f4ad60fad2e4ddb5c2b09b58b30a09df8b058c
Michael Kamau <tokamau@gmail.com>
2016-10-12 08:45:23 +0300

    Add definition of binary search

--- binary_search_src/binary_search.py
+++ binary_search_src/binary_search.py
@@ -6,5 +6,24 @@ class BinarySearch(list):
         :param b: step or difference between consecutive values
         """
         self.length = a
-        for i in range(b, a*b + b, b):
+        for i in range(b, a * b + b, b):
             self.append(i)
+
+    def search(self, val):
+        res = {'count': 0}
+        array = sorted(self)
+        left = 0
+        right = len(self) - 1
+        while left <= right:
+            res['count'] += 1
+            middle = (left + right) / 2
+            if array[middle] < val:
+                left = middle + 1
+            elif array[middle] > val:
+                right = middle - 1
+            else:
+                res['index'] = middle
+                return res
+        # val not found
+        res['index'] = -1
+        return res
